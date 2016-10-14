274b1e7016393ccc4c5c996325983907f4d19cf6
Ketil Stadskleiv <ketil@bokbasen.no>
2016-10-14 10:43:31 +0200

    Added HttpOptions class for HTTP related constants

---
+++ src/Http/HttpRequestOptions.php
@@ -0,0 +1,16 @@
+<?php
+namespace Bokbasen\Http;
+
+class HttpRequestOptions
+{
+
+    const CONTENT_TYPE_HAL = 'application/hal+json';
+
+    const CONTENT_TYPE_XML = 'application/xml';
+
+    const CONTENT_TYPE_JPEG = 'image/jpeg';
+
+    const CONTENT_TYPE_AUDIO_MPEG = 'audio/mpeg';
+
+    const CONTENT_TYPE_PDF = 'application/pdf';
+}
